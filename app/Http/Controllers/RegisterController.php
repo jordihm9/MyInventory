542a4eeb1229fdc2e15bf930d60360993c202fff
Jordi Hernandez <jjordihm9@gmail.com>
2021-04-16 17:57:25 +0200

    backend registration: create and validate email and create user

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+use App\Models\User;
+use App\Models\Email;
+
+class RegisterController extends Controller
+{
+    /**
+     * Create a new email, set the verification code and send an email with it
+     * Receive an email
+     */
+    public function setEmail(Request $request)
+    {
+        // validate email before continuing
+        $validated = $request->validate([
+            'email' => ['required', 'email:filter', 'unique:emails']
+        ]);
+
+        // generate the verification code
+        $verificationCode = Str::random(6);
+
+        // get the email from the request
+        $email = $request->input('email');
+        
+        // create and insert a new email
+        $email = Email::create([
+            'email'=> $email,
+            'verification_code' => $verificationCode,
+        ]);
+
+        // send the email with the verification code
+    }
+
+    /**
+     * Receive an email and a verification code
+     * Compare the code set for the email and the code received
+     */
+    public function validateEmail(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => ['required', 'email:filter', 'exists:emails']
+        ]);
+
+        // get the verification code sent
+        $verificationCode = $request->input('verification_code');
+
+        // search and get the email on the database
+        $email = Email::where('email', $request->input('email'))->firstOrFail();
+
+        // compare if code sent is the same as the code for the email
+        if ($email->verification_code === $verificationCode) {
+            // update the email
+            $email->verified = true;
+            $email->verified_at = Carbon::now();
+            $email->save();
+        }
+    }
+
+    /**
+     * Receive and email, name, surnames, sex and password
+     */
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => ['required', 'email:filter', 'exists:emails'],
+            'name' => ['required'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        // get the data from the request
+        $email = $request->input('email');
+        $name = $request->input('name');
+        $surnames = $request->input('surnames');
+        $sex = $request->input('sex');
+        $password = $request->input('password');
+        
+        // set to null the data that can be null if its empty
+        $surnames = $surnames === '' ? null : $surnames;
+        $sex = $sex === '' ? null : $sex;
+
+        // hash the password
+        $hashedPassword = Hash::make($password);
+
+        // search and get the email from the database
+        $email = Email::where('email', $email)->firstOrFail();
+
+        // create a new user
+        $user = User::create([
+            'name' => $name,
+            'surnames' => $surnames,
+            'sex' => $sex,
+            'password' => $hashedPassword,
+            'email_id' => $email->id,
+        ]);
+    }
+}
